c0335a3f8b9414415071e6610f252af3707a4b5b
Elizabeth Scheidt <escheidt5@gmail.com>
2019-11-14 12:40:33 -0500

    refactored templates, made it so that a user can only has a button to complete a ticket if the looged in user is the user assigned to a ticket

--- bugTracker/views.py
+++ bugTracker/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponseRedirect, reverse
 
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+# from django.contrib import messages
 
 from django.contrib.auth.models import User
 from bugTracker.models import Ticket
@@ -14,14 +14,12 @@ def login_view(request):
     html = 'generic_form_view.html'
     if request.method == "POST":
         form = LoginForm(request.POST)
-
         if form.is_valid():
             data = form.cleaned_data
             user = authenticate(
                 username=data['username'],
                 password=data['password']
             )
-
         if user:
             login(request, user)
             return HttpResponseRedirect(request.GET.get(
@@ -59,19 +57,23 @@ def register_user_view(request):
 
 @login_required
 def index(request):
+    # html = 'index.html'
     html = 'index.html'
 
+    current_user_id = request.user.id
     new = Ticket.objects.filter(status='New')
     assigned = Ticket.objects.filter(status='In Progress')
     completed = Ticket.objects.filter(status='Done')
     invalid = Ticket.objects.filter(status='Invalid')
+    ticket_types = [
+        ('New Tickets', new),
+        ('In Progress', assigned),
+        ('Completed Tickets', completed),
+        ('Invalid Tickets', invalid)]
 
     return render(request, html, {
-        'new_tickets': new,
-        'assigned_tickets': assigned,
-        'completed_tickets': completed,
-        'invalid_tickets': invalid
-    })
+        'ticket_types': ticket_types,
+        'current_user_id': current_user_id})
 
 
 @login_required
@@ -97,12 +99,18 @@ def user_detail_view(request, id):
     filed = Ticket.objects.filter(filed_by=user)
     assigned = Ticket.objects.filter(assigned_to=user)
     completed = Ticket.objects.filter(completed_by=user)
+    ticket_types = [
+        ('Current tickets assigned to ', assigned),
+        ('Tickets filed by ', filed),
+        ('Tickets completed by ', completed)
+    ]
 
     return render(request, html, {
         'user': user,
         'filed': filed,
         'assigned': assigned,
-        'completed': completed})
+        'completed': completed,
+        'ticket_types': ticket_types})
 
 
 @login_required
@@ -110,8 +118,11 @@ def ticket_detail_view(request, id):
     html = 'ticket.html'
 
     ticket = Ticket.objects.get(pk=id)
+    current_user_id = request.user.id
 
-    return render(request, html, {'ticket': ticket})
+    return render(request, html, {
+        'ticket': ticket,
+        'current_user_id': current_user_id})
 
 
 @login_required
@@ -135,7 +146,7 @@ def unassign_ticket(request, id):
     ticket.status = 'New'
     ticket.assigned_to = None
     ticket.save()
-    return HttpResponseRedirect(reverse('homepage'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 @login_required
@@ -145,7 +156,7 @@ def uncomplete_ticket(request, id):
     ticket.assigned_to = ticket.completed_by
     ticket.completed_by = None
     ticket.save()
-    return HttpResponseRedirect(reverse('homepage'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 @login_required
@@ -154,7 +165,7 @@ def assign_ticket(request, id):
     ticket.status = 'In Progress'
     ticket.assigned_to = request.user
     ticket.save()
-    return HttpResponseRedirect(reverse('homepage'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     # html = 'generic_form_view.html'
 
     # instance = Ticket.objects.get(pk=id)
@@ -173,15 +184,15 @@ def assign_ticket(request, id):
 @login_required
 def complete_ticket(request, id):
     ticket = Ticket.objects.get(pk=id)
-    if request.user.id != ticket.assigned_to.id:
-        messages.add_message(request, messages.INFO,
-                             'You may not complete a ticket that is not assigned to you')  # noqa
-        return HttpResponseRedirect(reverse('homepage'))
+    # if request.user.id != ticket.assigned_to.id:
+    #     messages.add_message(request, messages.INFO,
+    #                          'You may not complete a ticket that is not assigned to you')  # noqa
+    #     return HttpResponseRedirect(reverse('homepage'))
     ticket.status = 'Done'
     ticket.completed_by = ticket.assigned_to
     ticket.assigned_to = None
     ticket.save()
-    return HttpResponseRedirect(reverse('homepage'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     # html = 'generic_form_view.html'
 
     # instance = Ticket.objects.get(pk=id)
@@ -205,7 +216,7 @@ def mark_ticket_invalid(request, id):
     ticket.completed_by = None
     ticket.assigned_to = None
     ticket.save()
-    return HttpResponseRedirect(reverse('homepage'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 @login_required
@@ -213,4 +224,4 @@ def make_invalid_ticket_valid(request, id):
     ticket = Ticket.objects.get(pk=id)
     ticket.status = 'New'
     ticket.save()
-    return HttpResponseRedirect(reverse('homepage'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
